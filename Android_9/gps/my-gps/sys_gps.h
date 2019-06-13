68ee474d8315e1ec8d68840ee32451568a8c2ca0
dkwork <dk_dowork@163.com>
2019-06-13 16:54:09 +0800

    my-gps Optimize the code

--- Android_9/gps/my-gps/sys_gps.h
+++ Android_9/gps/my-gps/sys_gps.h
@@ -1,3 +1,23 @@
+/******************************************************************************
+
+  Copyright (C), 2019-2029, DCN Co., Ltd.
+
+ ******************************************************************************
+  File Name     : sys_gps.h
+  Version       : Initial Draft
+  Author        : Kang
+  Created       : 2019/6/13
+  Last Modified :
+  Description   : sys_gps.c header file
+  Function List :
+  History       :
+  1.Date        : 2019/6/1
+    Author      : Kang
+    Modification: Created file
+
+******************************************************************************/
+
+
 #ifndef __SYS_GPS_H__
 #define __SYS_GPS_H__
 
@@ -24,46 +44,32 @@
 #include <hardware/gps.h>
 
 
-/****************************************************
-*	First to change "Baudrate"、"Uart-Port Path"	*
-****************************************************/
-//
-// GPS driver version
-//	
-#define	SYS_GNSS_DRIVER_LIB_VERSION	"SYS_GNSS_LIB_V0.0.1" 
-//
-// *Change Baudrate for your GPS device :
-//
-//	GPS Baudrate = 9600		==>		GPS_DEVICE_BAUDRATE = B9600
-// 	GPS Baudrate = 19200	==>		GPS_DEVICE_BAUDRATE = B19200
-//	GPS Baudrate = 38400	==>		GPS_DEVICE_BAUDRATE = B38400
-//	GPS Baudrate = 57600	==>		GPS_DEVICE_BAUDRATE = B57600
+#ifdef __cplusplus
+#if __cplusplus
+extern "C"{
+#endif
+#endif /* __cplusplus */
+
+// GPS driver version	
+#define	SYS_GNSS_DRIVER_LIB_VERSION	"SYS_GNSS_LIB_V0.0.1"
+
 //	GPS Baudrate = 115200	==>		GPS_DEVICE_BAUDRATE = B115200
-//
 #define GPS_DEVICE_BAUDRATE		B115200
-//
-// This "path" for your UART port number ==> mapping to "ttymxcX" ( X maybe is 1、2、3 ... )。
-//
+
+// This "path" for your UART port
 #define GPS_DEVICE_PATH		"/dev/ttyUSB1"
 
-#define		NMEA_MAX_SIZE  	168
+#define	 NMEA_MAX_SIZE 168
 
-#define		CMD_MAX_LEN		166
+#define	 CMD_MAX_LEN 166
 
 //#define  GPS_DEBUG //debug flag
-#undef	 GPS_DEBUG_TOKEN	
 
 #define  AIP_LOGI(...)   ALOGI(__VA_ARGS__)
 #define  AIP_LOGE(...)   ALOGE(__VA_ARGS__)
 
 #ifdef GPS_DEBUG
-//#  define  D(...)   ALOGD(__VA_ARGS__)
-//#define D(format,...) ALOGD(__FILE__"(%s:%d)"format"", __FUNCTION__, __LINE__, ##__VA_ARGS__)
-
 #define D(format,...) ALOGD("(%s:%d) "format" ", __FUNCTION__, __LINE__, ##__VA_ARGS__) 
-
-#define D(format,...) ALOGD("(%s:%d)"format"", __FUNCTION__, __LINE__, ##__VA_ARGS__) 
-
 #else
 #  define  D(...)   ((void)0)
 #endif
@@ -111,7 +117,6 @@ typedef struct {
     int                     fd;
     GpsCallbacks            callbacks;
     pthread_t               thread;
-//    pthread_t		    nmea_thread;//kang del
     int                     control[2];
     int                     fix_freq;
     sem_t                   fix_sem;
@@ -121,19 +126,11 @@ typedef struct {
 
 
 static GpsCallbacks* g_gpscallback = 0;
-
-static int gps_opentty(GpsState *state);
-static void	gps_closetty(GpsState *state);
-static void	gps_wakeup(GpsState *state);
-static void	gps_sleep(GpsState *state);
-static int gps_checkstate(GpsState *state);
 static GpsState  _gps_state[1];
 static GpsState *gps_state = _gps_state;
 static int lastcmd = 0;
 static int started    = 0;
 static const char prop[] = GPS_DEVICE_PATH;
-
-static unsigned char isValid_nmea_cmd_checksum( const char *recv_cmd );
 
 static char buff[800];
 
@@ -143,14 +140,7 @@ static int16_t gnss_GPS_used_prn_buf[GPS_MAX_SVS];
 static int16_t gnss_Other_used_prn_buf[GPS_MAX_SVS];
 
 
-/*****************************************************************/
-/*****************************************************************/
-/*****                                                       *****/
-/*****       N M E A   T O K E N I Z E R                     *****/
-/*****                                                       *****/
-/*****************************************************************/
-/*****************************************************************/
-
+//NMEA TOKEN
 typedef struct {
     const char*  p;
     const char*  end;
@@ -171,4 +161,71 @@ static NmeaReader  *reader;
 static int nn;
 static int ret;
 
+//function definition
+static int close_gps(struct hw_device_t *device);
+static double convert_from_hhmm( Token  tok );
+static int epoll_deregister( int  epoll_fd, int  fd );
+static int epoll_register( int  epoll_fd, int  fd );
+static int gps_checkstate(GpsState *s);
+static void gps_closetty(GpsState *s);
+static unsigned char gps_dev_calc_nmea_csum(char *msg);
+static void gps_dev_send(int fd, char *msg);
+static int gps_opentty(GpsState *state);
+static void gps_sleep(GpsState *state);
+static void gps_state_done( GpsState*  s );
+static void gps_state_init( GpsState*  state );
+static void gps_state_lock_fix(GpsState *state);
+static void gps_state_start( GpsState*  s );
+static void gps_state_stop( GpsState*  s );
+static void gps_state_thread( void*  arg );
+static void gps_state_unlock_fix(GpsState *state);
+static void gps_state_update_fix_freq(GpsState *s, int fix_freq);
+static void gps_wakeup(GpsState *state);
+static const GpsInterface* gps__get_gps_interface(struct gps_device_t* dev);
+static unsigned char isValid_nmea_cmd_checksum( const char *recv_cmd );
+static void nmea_reader_addc( NmeaReader* r, int  c );
+static int nmea_reader_get_timestamp(NmeaReader*  r, Token  tok, time_t *timestamp);
+static void nmea_reader_init( NmeaReader*  r );
+static void nmea_reader_parse( NmeaReader* r );
+static int nmea_reader_update_accuracy( NmeaReader* r, Token accuracy );
+static int nmea_reader_update_altitude( NmeaReader*  r,
+                                      Token        altitude,
+                                      Token        units );
+static int nmea_reader_update_bearing( NmeaReader* r, Token bearing );
+static int nmea_reader_update_cdate( NmeaReader*  r, Token  tok_d, Token tok_m, Token tok_y );
+static int nmea_reader_update_date( NmeaReader*  r, Token  date, Token  mtime );
+static int nmea_reader_update_latlong( NmeaReader*  r,
+                                     Token        latitude,
+                                     char         latitudeHemi,
+                                     Token        longitude,
+                                     char         longitudeHemi );
+static int nmea_reader_update_speed( NmeaReader* r, Token speed );
+static int nmea_reader_update_time( NmeaReader*  r, Token  tok );
+static int nmea_reader_update_timemap( NmeaReader* r, Token systime_tok, Token timestamp_tok);
+static void nmea_reader_update_utc_diff( NmeaReader*  r );
+static Token nmea_tokenizer_get( NmeaTokenizer*  t, int  index );
+static int nmea_tokenizer_init( NmeaTokenizer*  t, const char*  p, const char*  end );
+static int open_gps(const struct hw_module_t* module, char const* name, struct hw_device_t** device);
+static double str2float( const char*  p, const char*  end );
+static int str2int( const char*  p, const char*  end );
+static void sys_gps_cleanup(void);
+static void sys_gps_delete_aiding_data(GpsAidingData flags);
+static const void* sys_gps_get_extension(const char* name);
+static int sys_gps_init(GpsCallbacks* callbacks);
+static int sys_gps_inject_location(double latitude, double longitude, float accuracy);
+static int sys_gps_inject_time(GpsUtcTime time, int64_t timeReference, int uncertainty);
+static void sys_gps_set_fix_frequency(int freq);
+static int sys_gps_set_position_mode(GpsPositionMode mode, GpsPositionRecurrence recurrence,
+         									  uint32_t min_interval, uint32_t preferred_accuracy, uint32_t preferred_time);
+static int sys_gps_start();
+static int sys_gps_stop();
+
+#ifdef __cplusplus
+#if __cplusplus
+}
+#endif
+#endif /* __cplusplus */
+
+
 #endif /* __SYS_GPS_H__ */
+
